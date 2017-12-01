f60635f2526951b56a35b773b6775a6b70b2154c
cartersnowden <33707975+cartersnowden@users.noreply.github.com>
2017-12-01 11:12:41 +1100

    Upload cordova-plugin-serial-magic-gears
    Initial release

---
+++ cordova-plugin-serial-magic-gears/www/EPCWriter.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="/js/config.js" />
+/// <reference path="/js/helpers.js" />
+
+var exec = require("cordova/exec");
+rfidScanner = {
+
+	writeEpc: function (epc, onSuccess, onFailure) {
+		exec(onSuccess, onFailure, "EPCWriter", "writeEPC", [epc]);
+	}
+}
